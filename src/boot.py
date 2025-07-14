48dd252ceea8a23b406d16e6087cbcfcfd4a123e
Jose Irigon de Irigon <jose.irigon.de.irigon@sap.com>
2025-07-14 10:13:27 +0100

    reduzindo o codigo do boot

--- src/boot.py
+++ src/boot.py
@@ -1,20 +1,11 @@
 import ota
-import time
 
 try:
     ota.connect_wifi()
     if ota.is_update_needed():
-        files = ota.fetch_file_list()
-        if files:
-            for fname in files:
-                ota.download_file(fname)
-            print("OTA update complete. Rebooting...")
-            time.sleep(2)
-            machine.reset()
+        print("Updating...")
+        ota.update()
     else:
         print("No OTA update needed.")
 except Exception as e:
     print("OTA update failed:", e)
-
-    ota.update_local_version(ota.get_remote_version())
-
--- src/ota.py
+++ src/ota.py
@@ -34,52 +34,24 @@ def connect_wifi():
     else:
         raise RuntimeError("Wi-Fi connection failed.")
 
-def fetch_file_list():
-    try:
-        print("Fetching file list from GitHub...")
-        r = requests.get(GITHUB_API_URL, headers=headers)
-        if r.status_code == 200:
-            # Get all files of interest. Secrets.py is ignored, it should be updated locally
-            return [file['name'] for file in r.json() if file['type'] == 'file' and file['name'] != "secrets.py"]
-        else:
-            print(f"Failed to fetch file list: {r}")
-            return []
-    except Exception as e:
-        print("Error during file list fetch:", e)
-        return []
-
-def download_file(name):
-    try:
-        url = f"{RAW_BASE_URL}/{name}"
-        print(f"Downloading {url}...")
-        r = requests.get(url)
-        if r.status_code == 200:
-            with open(name, "w") as f:
-                f.write(r.text)
-            print(f"Updated: {name}")
-        else:
-            print(f"Failed to download {name}: status {r.status_code}, text: {r.text}")
-        r.close()
-    except Exception as e:
-        print(f"Error downloading {name}: {e}")
-
-def get_local_version():
-    try:
-        with open(LOCAL_VERSION_FILE) as f:
-            return f.read().strip()
-    except OSError:
-        return "0.0.0"
-
-def get_remote_version():
-    try:
-        response = requests.get(f'{RAW_BASE_URL}/{LOCAL_VERSION_FILE}?ts={time.time()}', headers=headers)
-        if response.status_code == 200:
-            return response.text.strip()
-    except Exception as e:
-        print(f"Failed to fetch remote version:", e)
-    return None
 
 def is_update_needed():
+    def get_local_version():
+        try:
+            with open(LOCAL_VERSION_FILE) as f:
+                return f.read().strip()
+        except OSError:
+            return "0.0.0"
+
+    def get_remote_version():
+        try:
+            response = requests.get(f'{RAW_BASE_URL}/{LOCAL_VERSION_FILE}?ts={time.time()}', headers=headers)
+            if response.status_code == 200:
+                return response.text.strip()
+        except Exception as e:
+            print(f"Failed to fetch remote version:", e)
+        return None
+
     local = get_local_version()
     remote = get_remote_version()
     print(f"Local version: {local}, Remote version: {remote}")
@@ -87,7 +59,44 @@ def is_update_needed():
         return False
     return remote > local  # simple string comparison, e.g. "1.2.1" > "1.1.9"
 
-def update_local_version(remote_version):
-    with open(LOCAL_VERSION_FILE, "w") as f:
-        f.write(remote_version)
+def update():
+    def fetch_file_list():
+        try:
+            print("Fetching file list from GitHub...")
+            r = requests.get(GITHUB_API_URL, headers=headers)
+            if r.status_code == 200:
+                # Get all files of interest. Secrets.py is ignored, it should be updated locally
+                return [file['name'] for file in r.json() if file['type'] == 'file' and file['name'] != "secrets.py"]
+            else:
+                print(f"Failed to fetch file list: {r}")
+                return []
+        except Exception as e:
+            print("Error during file list fetch:", e)
+            return []
+    
+    def download_file(name):
+        try:
+            url = f"{RAW_BASE_URL}/{name}"
+            print(f"Downloading {url}...")
+            r = requests.get(url)
+            if r.status_code == 200:
+                with open(name, "w") as f:
+                    f.write(r.text)
+                print(f"Updated: {name}")
+            else:
+                print(f"Failed to download {name}: status {r.status_code}, text: {r.text}")
+            r.close()
+        except Exception as e:
+            print(f"Error downloading {name}: {e}")
 
+    try:
+        files = fetch_file_list()
+        if files:
+            for fname in files:
+                download_file(fname)
+            print("OTA update complete. Rebooting...")
+            time.sleep(2)
+            
+            machine.reset()
+    except Exception as e:
+        print("OTA update failed:", e)
